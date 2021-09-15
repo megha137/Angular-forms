4746481df1a621d7f0468fa43be5fa9e423f668a
Megha <shelarmegha13@gmail.com>
2021-09-15 13:48:40 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-forms';
+  Submitted:boolean;
+  RegisterForm = new FormGroup({
+  Fname:  new FormControl('',Validators.required),
+  Lname: new FormControl('',Validators.required)})
+   updateProfile() {
+    this.RegisterForm.setValue({Fname: 'Megha',Lname: 'Shelar'})
+  }
+
 }
